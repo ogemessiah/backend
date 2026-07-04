997fc7f546f6300d640e20eff25b82f6ff6f63af
Onwuemerie Ogechi <onwuemerieogechi@Onwuemeries-Mac-mini.local>
2026-07-04 12:26:08 +0100

    debug

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,11 +1,5 @@
 const admin = require('firebase-admin');
 
-const processedKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
-console.log('Processed key starts:', processedKey.slice(0, 30));
-console.log('Processed key ends:', JSON.stringify(processedKey.slice(-40)));
-console.log('Processed key line count:', processedKey.split('\n').length);
-
-
 admin.initializeApp({
 
   credential:
